0d8dd452ceb4054de9bcc76f448451b0f4808427
SridharPandian <sripan172@gmail.com>
2021-06-29 11:02:02 -0400

    Bug fixes for Infrared camera module when running rosun for AR-tracker

---
+++ realsense2_description/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/robotlab/catkin_ws/src/realsense-ros/realsense2_description
+# Build directory: /home/robotlab/catkin_ws/src/realsense-ros/realsense2_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(_ctest_realsense2_description_nosetests_tests "/home/robotlab/catkin_ws/src/catkin_generated/env_cached.sh" "/usr/bin/python2" "/opt/ros/kinetic/share/catkin/cmake/test/run_tests.py" "/home/robotlab/catkin_ws/src/test_results/realsense2_description/nosetests-tests.xml" "--return-code" "\"/usr/bin/cmake\" -E make_directory /home/robotlab/catkin_ws/src/test_results/realsense2_description" "/usr/bin/nosetests-2.7 -P --process-timeout=60 --where=/home/robotlab/catkin_ws/src/realsense-ros/realsense2_description/tests --with-xunit --xunit-file=/home/robotlab/catkin_ws/src/test_results/realsense2_description/nosetests-tests.xml")
